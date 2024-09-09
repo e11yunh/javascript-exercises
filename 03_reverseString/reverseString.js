e3c435d6d11e041f3916a466161f5d23e21a68d0
Elliot Yun <e11yunh@gmail.com>
2024-09-09 14:02:46 +0800

    Complete JS exercises from 01 - 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split("");
+    const reversed_arr = arr.reverse();
+    
+    return reversed_arr.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
 const removeFromArray = function() {
-};
+    const args = [...arguments];
+    const input_arr = args[0];
+    const removed_items = args.slice(1);
 
+    let output_arr = [...input_arr];
+
+    for (item of removed_items) {
+        while (output_arr.includes(item)) {
+            // Locate the index of the item to be removed
+            const item_index = output_arr.indexOf(item);
+            output_arr.splice(item_index, 1);
+        }
+    }
+
+    return output_arr;
+
+}
+
+console.log(removeFromArray([1, 2, 2, 3], 2))
 // Do not edit below this line
 module.exports = removeFromArray;
