77aeb03a82af54cf67373785d20e591d8cd8b294
Elliot Yun <e11yunh@gmail.com>
2024-09-10 12:57:40 +0800

    complete exercsises 05 - 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 > 0 && num2 > 0 && (Number.isInteger(num1) && Number.isInteger(num2))) {
+        let total = 0;
+        if (num1 > num2) []
+        let end = Math.max(num1, num2), start = Math.min(num1, num2);
+        for (let i = start; i <= end; i++) {
+            total += i;
+        }
+    
+        return total;
+    } else return "ERROR";
 
 };
 
+console.log(sumAll(-42, -3));
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp_F) {
+  let temp_C = Math.ceil(((temp_F - 32) * (5/9)) * 10)/10;
+  return temp_C;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_C) {
+  let temp_F = Math.ceil((temp_C * 9/5 + 32) * 10)/10;
+  return temp_F;
 };
 
+
+console.log(convertToCelsius(-442));
+console.log(convertToFahrenheit(-228));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
